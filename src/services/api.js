6bc9ba8add9975985c4cea206bfb347bf0db638f
27Adityasahil <adityasahil24@gmail.com>
2025-07-04 09:03:44 +0530

    Updates

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://admin-bimal-hospital.onrender.com",
-  
+  // baseURL: "https://admin-bimal-hospital.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://admin-bimal-hospital.onrender.com",  
 });
 
 instance.interceptors.request.use((config) => {
